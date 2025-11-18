824f7a58d0ae90c8f905d50d5f3ec757daec2103
Sagar-311 <sagars3113@gmail.com>
2025-11-18 23:15:56 +0530

    toastify

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-
-      <Routes>
+    <>
+    <ToastContainer />
+    <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+    </Routes>
+    </>
+      
   );
 }
 
